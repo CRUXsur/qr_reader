6baf20532b09e3c42c0697c88ae6b0bbec8cc882
CRUXsur <marcojtorrico@hotmail.com>
2022-02-27 15:41:07 -0400

    Provider como gestor de estado

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'package:qr_reader/pages/home_page.dart';
 import 'package:qr_reader/pages/mapa_page.dart';
+import 'package:qr_reader/providers/ui_provider.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'QR READER',
-      initialRoute: 'home',
-      routes: {
-        'home': (_) => const HomePage(),
-        'mapa': (_) => const MapaPage(),
-      },
-      theme: ThemeData(
-        primaryColor: Colors.deepPurple,
-        floatingActionButtonTheme: const FloatingActionButtonThemeData(
-          backgroundColor: Colors.deepPurple,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => new UiProvider()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'QR READER',
+        initialRoute: 'home',
+        routes: {
+          'home': (_) => const HomePage(),
+          'mapa': (_) => const MapaPage(),
+        },
+        theme: ThemeData(
+          primaryColor: Colors.deepPurple,
+          floatingActionButtonTheme: const FloatingActionButtonThemeData(
+            backgroundColor: Colors.deepPurple,
+          ),
         ),
       ),
     );
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'package:qr_reader/pages/mapas_page.dart';
 import 'package:qr_reader/pages/direcciones_page.dart';
+
+import 'package:qr_reader/providers/ui_provider.dart';
+
 import 'package:qr_reader/widgets/custom_navigatorbar.dart';
 import 'package:qr_reader/widgets/scan_button.dart';
 
@@ -32,8 +36,10 @@ class HomePage extends StatelessWidget {
 class _HomePageBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //* Obtener el selected menu opt
+    final uiProvider = Provider.of<UiProvider>(context);
     //* Cambiar para mostrar la pagina respectiva
-    final currentIndex = 0;
+    final currentIndex = uiProvider.selectedMenuOpt;
 
     switch (currentIndex) {
       case 0:
