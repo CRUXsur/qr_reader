9085b3cdf359389a076caf82ca17ca7473fcb4bc
CRUXsur <marcojtorrico@hotmail.com>
2022-02-27 09:22:21 -0400

    Diseno de la aplicacion

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,14 +1,30 @@
 import 'package:flutter/material.dart';
 
+import 'package:qr_reader/widgets/custom_navigatorbar.dart';
+import 'package:qr_reader/widgets/scan_button.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: const Text('Historial'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.delete_forever),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: const Center(
         child: Text('Home Page'),
       ),
+      bottomNavigationBar: const CustomNavigationBar(),
+      floatingActionButton: const ScanButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
