9c0d78ed34ce46d3028ce8a87e89f4d34afbf21b
CRUXsur <marcojtorrico@hotmail.com>
2022-03-02 09:55:35 -0400

    ScanListProvider-Leer Scans de la base de datos

--- lib/providers/scan_list_provider.dart
+++ lib/providers/scan_list_provider.dart
@@ -17,4 +17,30 @@ class ScanListProvider extends ChangeNotifier {
       notifyListeners();
     }
   }
+
+  cargarScans() async {
+    final scans = await DBProvider.db.getTodosLosScans();
+    //reemplazar el lisatdo anterior por el nuevo listado!
+    this.scans = [...?scans];
+    notifyListeners();
+  }
+
+  cargarScansPorTipo(String tipo) async {
+    final scans = await DBProvider.db.getScansPorTipo(tipo);
+    //reemplazar el lisatdo anterior por el nuevo listado!
+    this.scans = [...?scans];
+    this.tipoSeleccionado = tipo;
+    notifyListeners();
+  }
+
+  borrarTodos() async {
+    await DBProvider.db.deleteAllScans();
+    this.scans = [];
+    notifyListeners();
+  }
+
+  borrarScanPorId(int id) async {
+    await DBProvider.db.deleteScan(id);
+    this.cargarScansPorTipo(this.tipoSeleccionado);
+  }
 }
