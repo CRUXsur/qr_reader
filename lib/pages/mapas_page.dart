40ab12f748f58803a79d7ea22d9dcef5e8af20d4
CRUXsur <marcojtorrico@hotmail.com>
2022-03-02 14:38:28 -0400

    Mostrar Scans UI - Sin base de datos

--- lib/pages/mapas_page.dart
+++ lib/pages/mapas_page.dart
@@ -3,8 +3,18 @@ import 'package:flutter/material.dart';
 class MapasPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Mapas Page'),
+    return ListView.builder(
+      itemCount: 10,
+      itemBuilder: (_, i) => ListTile(
+        leading: Icon(Icons.map, color: Theme.of(context).primaryColor),
+        title: const Text('httplsdjhflshf'),
+        subtitle: const Text('ID : 1'),
+        trailing: const Icon(
+          Icons.keyboard_arrow_right,
+          color: Colors.green,
+        ),
+        onTap: () => print('abrir algo....'),
+      ),
     );
   }
 }
