b5c31d3dea83ceefe870dfcf2fcf34180886a1b8
CRUXsur <marcojtorrico@hotmail.com>
2022-03-03 09:20:16 -0400

    Optimizar la pantalla de direcciones y mapas

--- lib/pages/direcciones_page.dart
+++ lib/pages/direcciones_page.dart
@@ -1,30 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:qr_reader/providers/scan_list_provider.dart';
+
+import 'package:qr_reader/widgets/scan_tiles.dart';
 
 class DireccionesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    //redibujo!
-    final scanListProvider = Provider.of<ScanListProvider>(context);
-    final scans = scanListProvider.scans;
-
-    return ListView.builder(
-      itemCount: scans.length,
-      itemBuilder: (_, i) => ListTile(
-        leading: Icon(
-          Icons.home_outlined,
-          color: Theme.of(context).primaryColor,
-        ),
-        title: Text(scans[i].valor),
-        subtitle: Text(scans[i].id.toString()),
-        trailing: const Icon(
-          Icons.keyboard_arrow_right,
-          color: Colors.green,
-        ),
-        onTap: () => print(scans[i].id),
-      ),
-    );
-    ;
+    return const ScanTiles(tipo: 'http');
   }
 }
