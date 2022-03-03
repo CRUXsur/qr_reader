dc04c5bb57ce2f4e958a595b5ce3c308df11f5e0
CRUXsur <marcojtorrico@hotmail.com>
2022-03-03 16:41:19 -0400

    Mostrar GoogleMap

--- lib/pages/mapa_page.dart
+++ lib/pages/mapa_page.dart
@@ -1,12 +1,26 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import 'package:qr_reader/providers/db_provider.dart';
 
-class MapaPage extends StatelessWidget {
+class MapaPage extends StatefulWidget {
   const MapaPage({Key? key}) : super(key: key);
 
   @override
+  State<MapaPage> createState() => _MapaPageState();
+}
+
+class _MapaPageState extends State<MapaPage> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  @override
   Widget build(BuildContext context) {
+    final CameraPosition puntoInicial = CameraPosition(
+      target: LatLng(37.42796133580664, -122.085749655962),
+      zoom: 14.4746,
+    );
+
     //! recibo lo que estoy mandando como argumento del navigator pushNamed!
     //? HACER UN CASTING:
     //* final ScanModel scan = ModalRoute.of(context)!.settings.arguments
@@ -17,8 +31,12 @@ class MapaPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Mapa'),
       ),
-      body: Center(
-        child: Text(scan.valor),
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: puntoInicial,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
       ),
     );
   }
